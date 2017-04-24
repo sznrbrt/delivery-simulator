70f93a6c9c0a3ad4b626f8aa42cbd98cf42cf518
Norbert Szucs <szucs.norbert@outlook.com>
2017-04-24 22:28:58 +0200

    Add feat: Vehicle finds way to home automatically

--- Restaurant.js
+++ Restaurant.js
@@ -7,20 +7,33 @@ function Restaurant(x, y) {
   this.openOrders = [];
   this.waitingOrders = [];
   this.closedOrders = [];
-  this.vehicle = new Vehicle(23, 6, this.pickupPosition.x, this.pickupPosition.y);
+  this.vehicle = new Vehicle(3, 2, this.pickupPosition.x, this.pickupPosition.y);
 }
 
 Restaurant.prototype.update = function (delta) {
+  if(this.openOrders.length > 0) {
+    this.waitingOrders = this.openOrders;
+    this.openOrders = [];
+  }
+
+
+  // console.log(this.pickupPosition);
+  // console.log(this.vehicle.position);
+  // console.log(this.vehicle.position.equals(this.pickupPosition));
+  if(this.waitingOrders.length > 0 && this.vehicle.position.equals(this.pickupPosition)) {
+    this.vehicle.assignTask(this.waitingOrders[0]);
+    this.waitingOrders.shift()
+  }
+
   this.vehicle.update();
 };
 
 Restaurant.prototype.draw = function () {
-  Canvas2D.canvasContext.font="14px Georgia";
-  Canvas2D.canvasContext.fillStyle="white";
-  Canvas2D.canvasContext.fillText(this.openOrders.length, this.canvasPosition.x + 15, this.canvasPosition.y - 15);
-
   Canvas2D.canvasContext.fillStyle = "red";
   Canvas2D.canvasContext.fillRect(this.canvasPosition.x, this.canvasPosition.y, 40, 40);
+  Canvas2D.canvasContext.font="14px Georgia";
+  Canvas2D.canvasContext.fillStyle="white";
+  Canvas2D.canvasContext.fillText(this.waitingOrders.length, this.canvasPosition.x + 15, this.canvasPosition.y + 25);
 
   this.vehicle.draw();
 };
--- Vehicle.js
+++ Vehicle.js
@@ -1,39 +1,72 @@
 "use strict";
 
 function Vehicle(_x, _y, resX, resY) {
-  this.position = { x: _x, y: _y};
+  this.position = new Vector2(_x, _y);
   this.canvasPosition = new Vector2(_x * 40, _y * 40);
   this.restaurantPickupPosition = { x: resX, y: resY };
-  this.speed = 0.05;
-  this.tasks = [{ name: 'Go to restaurant', start: { x: _x, y: _y}, end: { x: resX, y: resY }, route: [] }];
+  this.speed = 2.5;
+  this.tasks = [];
+  this.completedTasks = [];
+  this.followedPath = [];
 }
 
 Vehicle.prototype.update = function (delta) {
+  // Check if there is a task - if so, check if route is available -> IF NOT GET SHORTEST PATH
   if(this.tasks.length > 0 && this.tasks[0].route.length === 0) {
     this.tasks[0].route = this.getShortestPath(this.tasks[0].start, this.tasks[0].end);
   }
-  if(this.tasks.length > 0 && this.tasks[0].route.length > 0) {
-    // Start movement and update position;
-    if(this.position.x === this.tasks[0].route[0][0] && this.position.y === this.tasks[0].route[0][1]) {
-      this.tasks[0].route.shift();
+
+  // Check if there is a task if so do, if not, go home
+  if(this.tasks.length > 0) {
+
+    // Movement for the task
+  } else {
+    // Check if home - if so, return
+    if(this.canvasPosition.x * 40 === this.restaurantPickupPosition.x * 40 &&
+       this.canvasPosition.y * 40 === this.restaurantPickupPosition.y * 40 ) {
+
+      console.log('HOME');
+      return;
+    }
+    // Movement for going home
+    var path = this.followedPath.length === 0 ? this.getShortestPath(this.position, this.restaurantPickupPosition) : this.followedPath;
+
+    this.followedPath.length === 0 ? this.followedPath = path : null;
+
+    var targetX = path[0][0];
+    var targetY = path[0][1];
+
+    // If equals to current pos, node reached
+    if(targetX * 40 === this.canvasPosition.x && targetY * 40 === this.canvasPosition.y) {
+      if(this.followedPath.length === 1) return;
+      this.followedPath.shift(); // If reached get next target
     } else {
-      if(this.position.x !== this.tasks[0].route[0][0]) {
-        // Vertical movement
-      }
-      else if(this.position.y !== this.tasks[0].route[0][1]) {
-        // Horizontal movement
-        var targetY = this.tasks[0].route[0][1] * 40;
-        this.canvasPosition.y += (targetY - this.canvasPosition.y) * this.speed;
+      // Not equals
+      // If X not equals move horizontally, else vertically
+      if(targetX * 40 !== this.canvasPosition.x) {
+        //Move horizontally
+        if(targetX * 40 - this.canvasPosition.X > 0) this.canvasPosition.x -= this.speed;
+        else this.canvasPosition.x += this.speed;
+      } else if(targetY * 40 !== this.canvasPosition.y) {
+        //Move vertically
+        if(targetY * 40 - this.canvasPosition.y > 0) this.canvasPosition.y += this.speed;
+        else this.canvasPosition.y -= this.speed;
       } else {
-        console.log('ERROR VAN');
+        console.log('WHERE?');
       }
     }
   }
 };
 
 Vehicle.prototype.draw = function () {
+  // console.log(this.position);
+  // console.log(this.canvasPosition);
   Canvas2D.canvasContext.fillStyle = "red";
   Canvas2D.canvasContext.fillRect(this.canvasPosition.x + 15, this.canvasPosition.y + 15, 10, 10);
+};
+
+Vehicle.prototype.assignTask = function (data) {
+  this.tasks.push(data);
 };
 
 Vehicle.prototype.getShortestPath = function (a, b) {
